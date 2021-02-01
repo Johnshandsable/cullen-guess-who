b950d4985678587dd4aa79d7b9036ebfd82628dc
Johnshandsable <Jshands@asu.edu>
2021-02-01 16:49:46 -0600

    display the guess of the guess who game

--- client.js
+++ client.js
@@ -5,12 +5,14 @@ console.log('js working');
 $(document).ready(function () {
   console.log('jquery loaded');
 
+  // render a random user
+  $('body').append(`
+    <h1 id="GUESS WHO THIS IS!">${returnName()}</h1>
+  `);
   // Generates user names and githubUsernames, but uses a default picture
   for (let peep of people) {
     $('body').append(`
     <div class="profile" data-index=${people.index}>
-      ${peep.name}
-      ${peep.githubUsername}
       <img src="https://github.com/${peep.githubUsername}.png?size=250" alt="Profile image of ${peep.name}">
     </div>
     `);
@@ -21,10 +23,13 @@ $(document).ready(function () {
 
 function checkUserWasRight() {
   console.log("what's happening");
+  returnName;
 }
 
 // minimum number of people is 1 and max is 7
-function randomNumber(min = 1, max = 7) {
+function returnName(min = 1, max = 7) {
   let index = Math.floor(Math.random() * (1 + max - min) + min);
-  return people[index];
+  console.log(index);
+  console.log(people[index].name);
+  return people[index].name;
 }
